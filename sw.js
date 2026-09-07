a31f31319b804402167cb27c3e99fa647cbb436c
facebukwork-beep <facebukwork@gmail.com>
2026-09-07 17:09:32 +0530

    Bump mobile cache for Blocked Videos center

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iap-mobile-v19-smart-performance-3';
+const CACHE='iap-mobile-v19-blocked-center-4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./v19.css','./v19.js','./dispute-helper.js','./manifest.webmanifest','./icon48.png','./icon128.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
